8077874327f740158edf656f3cea3b11f53e2294
Cameron Smith <cameron.ray.smith@gmail.com>
2025-10-05 11:41:34 -0400

    feat(nix): add main flake configuration

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "test-starlight documentation site with Astro Starlight and Bun";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.flake = false;
+  };
+
+  outputs =
+    inputs@{
+      self,
+      flake-parts,
+      nixpkgs,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      imports = with builtins; map (fn: ./nix/modules/${fn}) (attrNames (readDir ./nix/modules));
+    };
+}
