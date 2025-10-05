0bd3b30f9214aa7fd6f91a9b09b24249aeb74ecc
Cameron Smith <cameron.ray.smith@gmail.com>
2025-10-05 11:45:36 -0400

    feat(nix): add docs build package

--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -13,9 +13,31 @@
     }:
     {
       # nix build
-      packages = {
-        # Default package - will add docs build later
-        # For now, set-git-env is available from git-env module
+      packages = rec {
+        docs = pkgs.buildNpmPackage {
+          pname = "test-starlight-docs";
+          version = "0.0.1";
+
+          src = ../..;
+
+          buildInputs = with pkgs; [
+            vips
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          npmDepsHash = lib.fakeSha256;
+
+          installPhase = ''
+            runHook preInstall
+            cp -pr --reflink=auto dist $out/
+            runHook postInstall
+          '';
+        };
+
+        default = docs;
       };
     };
 }
