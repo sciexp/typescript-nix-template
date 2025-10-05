78a7809e67898f2079c81a9c6febaa17175913cf
Cameron Smith <cameron.ray.smith@gmail.com>
2025-10-05 11:41:17 -0400

    feat(nix): add development shell configuration

---
+++ nix/modules/devshell.nix
@@ -0,0 +1,35 @@
+{
+  inputs,
+  ...
+}:
+{
+  perSystem =
+    {
+      config,
+      self',
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      devShells = {
+        default = pkgs.mkShell {
+          name = "test-starlight-dev";
+          inputsFrom = [ config.pre-commit.devShell ];
+          packages = with pkgs; [
+            bun
+            nodejs
+            just
+            git
+            config.packages.set-git-env
+          ];
+
+          shellHook = ''
+            export REPO_ROOT=$(git rev-parse --show-toplevel 2>/dev/null || pwd)
+            set-git-env
+            printf "\n$GIT_REPO_NAME $GIT_REF $GIT_SHA_SHORT\n\n"
+          '';
+        };
+      };
+    };
+}
